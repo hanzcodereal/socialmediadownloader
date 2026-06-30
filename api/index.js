411aeabe72786fd3ecbe9b60c99e61c979010281
hanzcodereal <hanzzcodee@gmail.com>
2026-07-01 01:13:42 +0700

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import tiktokApp from './tiktok.js';
+import instagramApp from './instagram.js';
+import spotifyApp from './spotify.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/tiktok', tiktokApp);
+app.use('/api/instagram', instagramApp);
+app.use('/api/spotify', spotifyApp);
+
+app.get('/', (req, res) => {
+  res.json({
+    status: true,
+    message: 'Social Media Downloader API',
+    platforms: ['tiktok', 'instagram', 'spotify']
+  });
+});
+
+export default app;
